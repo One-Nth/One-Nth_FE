735d3b2fff1545ec658fdeb7edb4a3338e484ea0
heejung <mishel7@naver.com>
2025-07-22 21:08:32 +0900

    [feat] : Main Toolbar 삭제

--- app/src/main/java/com/example/onenthapp/AlarmActivity.kt
+++ app/src/main/java/com/example/onenthapp/AlarmActivity.kt
@@ -3,6 +3,7 @@ package com.example.onenthapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.onenthapp.databinding.ActivityAlarmBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlarmActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAlarmBinding
@@ -12,11 +13,17 @@ class AlarmActivity : AppCompatActivity() {
         binding = ActivityAlarmBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 뒤로가기 버튼
-        binding.btnLeft.setOnClickListener {
-            finish()
-        }
+        val adapter = AlarmPagerAdapter(this)
+        binding.viewPager.adapter = adapter
 
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "N분의 1"
+                1 -> "꿀팁 N분의 1"
+                else -> ""
+            }
+        }.attach()
 
+        binding.btnLeft.setOnClickListener { finish() }
     }
 }
