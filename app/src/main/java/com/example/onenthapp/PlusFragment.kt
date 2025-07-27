7d1611cd3af04c44451bccaf881e6c4d8f01938c
sti1andy <cjdrbf22@gmail.com>
2025-07-28 04:38:55 +0900

    [Design] 상품등록 화면 완성
    - 같이 사요 상품 등록 구현
    - 토글 버튼 로직 구현
    - edittext 라이브러리 수정
    
    #31

--- app/src/main/java/com/example/onenthapp/PlusFragment.kt
+++ app/src/main/java/com/example/onenthapp/PlusFragment.kt
@@ -1,17 +1,46 @@
 package com.example.onenthapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.onenthapp.databinding.FragmentPlusBinding
 
 class PlusFragment : Fragment() {
+    private var _binding: FragmentPlusBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_plus, container, false)
-        return view
+    ): View {
+        _binding = FragmentPlusBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToggleButtons()
+    }
+
+    private fun setupToggleButtons() {
+        binding.btnConfirmYes.setOnClickListener {
+            binding.btnConfirmYes.isChecked = true
+            binding.btnConfirmNoCancel.isChecked = false
+        }
+        binding.btnConfirmNoCancel.setOnClickListener {
+            binding.btnConfirmNoCancel.isChecked = true
+            binding.btnConfirmYes.isChecked = false
+        }
+        binding.btnWay1.setOnClickListener {
+            binding.btnWay1.isChecked = true
+            binding.btnWay2.isChecked = false
+        }
+        binding.btnWay2.setOnClickListener {
+            binding.btnWay2.isChecked = true
+            binding.btnWay1.isChecked = false
+        }
     }
 }
