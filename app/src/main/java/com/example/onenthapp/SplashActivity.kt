43e74aace94688cec895356b9bcd3a75aca53708
yujjj23 <bt26aza2018@gmail.com>
2025-08-04 18:24:32 +0900

    [Fix] 첫화면 UI 수정

--- app/src/main/java/com/example/onenthapp/SplashActivity.kt
+++ app/src/main/java/com/example/onenthapp/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.onenthapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,8 +12,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val tvLogin = findViewById<TextView>(R.id.tv_login)
-        val tvSignup = findViewById<TextView>(R.id.tv_signup)
+        val tvLogin = findViewById<ImageButton>(R.id.emailLoginBtn)
+        val tvSignup = findViewById<ImageButton>(R.id.signupBtn)
+        val tvFindAccount = findViewById<TextView>(R.id.findAccountText) // 계정찾기 텍스트뷰 추가
 
         tvLogin.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
@@ -20,6 +22,10 @@ class SplashActivity : AppCompatActivity() {
 
         tvSignup.setOnClickListener {
             startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        tvFindAccount.setOnClickListener {
+            startActivity(Intent(this, FindAccountActivity::class.java))
         }
     }
 }
