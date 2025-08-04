9251826a002c3ff113c0dcc106fd39ffffdbbe94
yujjj23 <bt26aza2018@gmail.com>
2025-08-05 08:02:00 +0900

    [Feat] 거래후기 화면 연결

---
+++ app/src/main/java/com/example/onenthapp/WriteReviewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onenthapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WriteReviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_review) // XML 연결
+    }
+}
